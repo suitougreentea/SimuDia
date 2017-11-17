c2cc8056c21a118b89a16000ef905e3a1523355b
suitougreentea <suitougreentea@users.noreply.github.com>
2017-11-17 10:00:29 +0900

    Emit error and exit if no file specified

--- Program.cs
+++ Program.cs
@@ -14,6 +14,12 @@ namespace simutrans_diagram
         [STAThread]
         static void Main()
         {
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length == 1)
+            {
+                MessageBox.Show("Drop timetable file to application or pass it by command-line argument.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
